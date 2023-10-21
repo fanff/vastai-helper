d416b121dfaa3b65143948695ce95ccd03f56412
fanf <fanf@fanf>
2023-10-21 17:19:34 +0200

    updating

--- src/vastai_helper/scan.py
+++ src/vastai_helper/scan.py
@@ -8,36 +8,35 @@ import sys
 
 import click
 from vastai import vast
+p:ArgumentParser = vast.parser.parser
+p.register
 
-
-
-
+print(vast.__file__)
 def list_instances(vastai_exe):
     res = subprocess.check_output(f"{vastai_exe} show instances --raw ", shell=True)
     json_res = json.loads(res)
     return json_res
 
-def list_instance_matching_format(query):
-    cmd = f"vastai search offers '{query}' -o 'dph' --raw"
+def list_instance_matching_format(vastai_exe,query):
+
+    
+    print("used q",query)
+    cmd = f"{vastai_exe} search offers '{query}' --raw "
+    print("used command",cmd)
     res = subprocess.check_output(cmd, shell=True)
     insts = json.loads(res)
     return insts
     
-def rent_machine(machine_id,
+def rent_machine(vastaiexe,machine_id,
                 storage=30.28,
                 image:str="pytorch/pytorch",
                 start_script:str=""):
     
-    
-    # 
-    cmd = f"vastai create instance {machine_id} --image {image} --disk {storage} --ssh --direct --onstart {start_script} --raw"
-
-
+    cmd = f"{vastaiexe} create instance {machine_id} --image {image} --disk {storage} --ssh --direct --onstart {start_script} --raw"
 
     print("used command",cmd)
     res = subprocess.check_output(cmd, shell=True)
     print("response ",res.decode("utf-8"))
-
     insts = json.loads(res)
     return insts
 
@@ -52,9 +51,9 @@ def cli():
     pass
 
 
-@cli.command()
+@cli.command(help="""generate pretty ssh configuration to use with existing instances""")
 @click.option("--vastaiexe",type=str,default="vastai",help="vastaiexe to use")
-@click.option("--vastaikey",type=str,default="~/.ssh/id_rsa_vast.ai",help="vastai key to use")
+@click.option("--vastaikey",type=str,default="~/.ssh/id_rsa_vast.ai",help="vastai sshkey to use")
 def ssh_config(vastaiexe,vastaikey):
     
     ssh_template = """
@@ -102,15 +101,16 @@ gpu_name==RTX_3090 num_gpus==1 rented=False verified=True
 @click.option("--start_script",type=str,required=True,help="script to run on start")
 @click.option("--storage",type=float,default=100,help="storage to allocate")
 @click.option("--image",type=str,default="pytorch/pytorch:latest",help="image to use")
-def rent(query,start_script,storage,image):
-    insts = list_instance_matching_format(query+f" disk_space>{int(storage)*2}")
+@click.option("--vastaiexe",type=str,default="vastai",help="vastaiexe to use")
+def rent(query,start_script,storage,image,vastaiexe):
+    insts = list_instance_matching_format(vastaiexe,query+f" disk_space>{int(storage)*2}")
     print("found %s instances"%len(insts))
 
     selected = insts[0]
 
     print("selected instance:",selected["id"],selected["gpu_name"],selected["num_gpus"])
 
-    renting_response = rent_machine(selected["id"],
+    renting_response = rent_machine(vastaiexe,selected["id"],
                 storage=storage,
                 image=image,
                 start_script=start_script)
@@ -118,13 +118,14 @@ def rent(query,start_script,storage,image):
 
 
 @cli.command(help="""list available instances matching query\n Example:\n\n \
-gpu_name==RTX_3090 num_gpus==1 rented=False verified=True disk_space>100 
+gpu_name==RTX_3090 num_gpus==1 rented=False verified=True disk_space>=100 
              """)
+@click.option("--vastaiexe",type=str,default="vastai",help="vastaiexe to use")
 @click.option("--query",
-              default="gpu_name==RTX_3090 num_gpus==1 rented=False verified=True disk_space>100",
+              default="gpu_name==RTX_3090 num_gpus==1 rented=False verified=True disk_space>=100",
               help="query to search for")
-def query(query):
-    insts = list_instance_matching_format(query)
+def query(vastaiexe,query):
+    insts = list_instance_matching_format(vastaiexe,query)
     selection = ["id","score","num_gpus","gpu_name","disk_space","dph_total"]
     print("\t".join(selection))
     for selected in insts:
